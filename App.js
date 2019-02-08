40cd109f4ff4aecdc3379e58ca1aef468ae163f2
chidera <dexiouz@gmail.com>
2019-02-08 13:41:35 +0100

    COmpleted likes App and presentational component

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PresentationalComponent from './components/PresentationalComponent';
+import Likes from './components/Likes';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <PresentationalComponent title="Presentational-state-prop"/>
+        <Likes />
       </View>
     );
   }
